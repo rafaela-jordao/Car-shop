763a9cc7533f8d1cb0d04eb9978953674623f191
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-09-14 15:59:51 -0300

    criando a interface IVehicle

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,15 @@
+import { z } from 'zod';
+
+const VehicleZodSchema = z.object({
+  model: z.string().min(3, { message: 'Must be 3 or more characters long' }),
+  year: z.number()
+    .int()
+    .positive()
+    .gte(1900)
+    .lte(2022),
+  color: z.string().min(3, { message: 'Must be 3 or more characters long' }),
+  status: z.boolean().optional(),
+  buyValue: z.number().int(),
+});
+
+export type IVehicle = z.infer<typeof VehicleZodSchema>;
